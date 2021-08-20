b11b861486f20d9e18040c5cccb5251f0e87ff74
Bruno <bruno.nascimentl@frontmen.nl>
2021-08-20 15:33:58 +0200

    create table to show imported file and add root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+// TODO: add type (currently TS doesn't support Set)
+const toFindDuplicates = (data: any) => {
+  const duplicates = new Set(
+    data.filter((item: string, index: number) => data.indexOf(item) !== index)
+  );
+
+  return [...duplicates];
+};
+
+export interface ParsedImportedFile {
+  Reference: string;
+  ["Account Number"]: string;
+  Description: string;
+  ["Start Balance"]: string;
+  ["Mutation"]: string;
+  ["End Balance"]: string;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'app';
+  title = "app";
+  data: ParsedImportedFile[] = [];
+  displayedColumns: string[] = [
+    "Reference",
+    "Account Number",
+    "Description",
+    "Start Balance",
+    "Mutation",
+    "End Balance",
+  ];
+
+  // TODO: add type (currently TS doesn't support Set)
+  duplicateReferences: any[] = [];
+  invalidTransactionReferences: string[] = [];
+  generateReportClicked = false;
+
+  importXML(data: ParsedImportedFile[]) {
+    this.resetState();
+    this.data = data;
+  }
+
+  importCSV(data: ParsedImportedFile[]) {
+    this.resetState();
+    this.data = data;
+  }
+
+  resetState() {
+    this.duplicateReferences = [];
+    this.invalidTransactionReferences = [];
+    this.generateReportClicked = false;
+  }
+
+  generateReport() {
+    this.resetState();
+    this.generateReportClicked = true;
+
+    this.data.forEach((item) => {
+      const sum = Number(item["Start Balance"]) + Number(item["Mutation"]);
+      const reportedEndBalance = Number(item["End Balance"]);
+      if (Number(sum.toFixed(2)) !== Number(reportedEndBalance.toFixed(2))) {
+        this.invalidTransactionReferences.push(item["Reference"]);
+      }
+    });
+
+    this.duplicateReferences = toFindDuplicates(
+      this.data.map((item) => item["Reference"])
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { FileUploadComponent } from "./file-upload/file-upload.component";
+import { MaterialModule } from "./material.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TransactionReportComponent } from './transaction-report/transaction-report.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FileUploadComponent, TransactionReportComponent],
+  imports: [BrowserModule, MaterialModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
